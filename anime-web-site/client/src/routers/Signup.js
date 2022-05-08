01c653366c1e9563c1ba3ad80d78f56af57ed711
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2022-05-08 19:38:45 +0300

    Update Signup.js

--- anime-web-site/client/src/routers/Signup.js
+++ anime-web-site/client/src/routers/Signup.js
@@ -25,7 +25,7 @@ function Signup({setProfile}) {
             return;
         }
         console.log(e.target)
-        axios.post("http://localhost:5000/u/signup",{
+        axios.post("https://anime-sed.herokuapp.com/u/signup",{
             "img":null,
             "name": e.target.name.value,
             "surname": e.target.surname.value,
